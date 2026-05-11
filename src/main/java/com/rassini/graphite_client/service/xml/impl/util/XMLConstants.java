9180130e125f542b9f263dbe0100ff257ef72617
nitsugamg83 <nitsugamg@outlook.com>
2026-05-11 11:39:04 -0600

    agregar constantes para xmls

---
+++ src/main/java/com/rassini/graphite_client/service/xml/impl/util/XMLConstants.java
@@ -0,0 +1,17 @@
+package com.rassini.graphite_client.service.xml.impl.util;
+
+public class XMLConstants {
+
+    public final String CERO="0";
+    public final String PROVEEDOR="PROVEEDOR";
+    public final String ROW_ID="0x000000000005d382";
+    public final String CONTACT_ROW_ID="0x000000000005e6c1";
+    public final String CONTACT_MALE="MALE";
+    public final String PARENT_ROW_ID="0x000000000005dfc3";
+    public final String LAST_MODIFIED_USER="mfg";
+    public final String LAST_MODIFIED_TIME="46780";
+    public final String ADDRESS_LOGIC_KEY="46780";
+    public final String CONTEXT_VERSION="9,2";
+    public final String LANG_CODE="ls";
+
+}
